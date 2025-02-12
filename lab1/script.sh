19966b56ca8691eeb305718886ff2d7a85cd38d7
root <toldan04@yandex.ru>
2025-02-12 21:35:32 +0300

    7/18

--- lab1/script.sh
+++ lab1/script.sh
@@ -1,14 +1,26 @@
 #!bin/bash
 
 # 1
-W3="work3.log"
-awk -F: '{printf "user %s has id %s\n", $1, $3}' /etc/passwd > $W3
-printf '\n' >> $W3
+OUT="work3.log"
+awk -F: '{printf "user %s has id %s\n", $1, $3}' /etc/passwd > $OUT
+echo >> $OUT
 
 # 2
-chage -l root | grep 'Последний раз' >> $W3
-printf '\n' >> $W3
+chage -l root | grep 'Последний раз' >> $OUT
+echo >> $OUT
 
 # 3
-cut -d: -f1 /etc/group | paste -sd, >> $W3
-printf '\n' >> $W3
+cut -d: -f1 /etc/group | paste -sd, >> $OUT
+echo >> $OUT
+
+# 4
+echo "Be careful!" > /etc/skel/readme.txt
+
+# 5
+useradd -p 12345678 u1
+
+# 6
+groupadd g1
+
+# 7
+usermod -aG g1 u1
